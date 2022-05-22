3dc0ae30503f9141413a62eef01e314b77b3f34e
Yousef500 <Kazuya14725@gmail.com>
2022-05-22 20:06:28 +0300

    finished 19 in 14

--- components/card/card.js
+++ components/card/card.js
@@ -1,35 +1,38 @@
 import Image from "next/image";
-import { useState } from "react";
+import {useState} from "react";
 import styles from "./card.module.css";
+import {motion} from "framer-motion"
+import cls from "classnames";
 
-const Card = ({ imgUrl, size = "medium" }) => {
+const Card = ({imgUrl = "", size = "medium"}) => {
 
     const [imgSrc, setImgSrc] = useState(imgUrl)
 
-  const classMap = {
-    large: styles.lgItem,
-    medium: styles.mdItem,
-    small: styles.smItem,
-  };
+    const classMap = {
+        large: styles.lgItem,
+        medium: styles.mdItem,
+        small: styles.smItem,
+    };
 
-  const handleOnError = (e) => {
-    console.log("hi error");
-    setImgSrc("/static/clifford.webp")
-  };
+    const handleOnError = () => {
+        console.log("hi error");
+        setImgSrc("https://images.unsplash.com/photo-1485846234645-a62644f84728?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1159")
+    };
 
-  return (
-    <div className={styles.container}>
-      <div className={classMap[size]}>
-        <Image
-          src={imgSrc}
-          alt="image"
-          layout={"fill"}
-          className={styles.cardImg}
-          onError={handleOnError}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <motion.div className={cls(classMap[size], styles.imgMotionWrapper)} whileHover={{scale: 1.1}}>
+                <Image
+                    src={imgSrc}
+                    alt="image"
+                    layout={"fill"}
+                    className={styles.cardImg}
+                    onError={handleOnError}
+                    priority="high"
+                />
+            </motion.div>
+        </div>
+    );
 };
 
 export default Card;
